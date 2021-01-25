2f1db2baf738a79e10e9fefb4fb6fc647122939a
Tamzidul Matin <41488579+tmatin100@users.noreply.github.com>
2021-01-25 10:30:09 -0500

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -7,20 +7,4 @@ resource "aws_instance" "Jenkins" {
     tags = {
       Name = element(var.tags,count.index)
    }
-    /*provisioner "remote-exec" {
-      inline = [
-         "yum update -y",
-         "sudo yum install openjdk-8-jdk -y",
-         " sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
-  sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-         "sudo yum update -y",
-         "sudo yum install jenkins -y",
-         "sudo systemctl start jenkins",
-         "sudo systemctl enable jenkins" ]
-       connection {
-        type = "ssh"
-        user = "ubuntu"
-        key_name = "WDL"
-        host = self.public_ip
-    } */
-  }
+    
